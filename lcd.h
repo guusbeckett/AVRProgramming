5f230df2e885fd24cee5c0a068770fa1c7d62303
loljimmo12 <spongebob.958@gmail.com>
2014-02-12 19:25:08 +0000

    added lcd

---
+++ lcd.h
@@ -0,0 +1,6 @@
+void lcdCommand ( unsigned char command );
+void lcdWriteChar( unsigned char dat );
+void init_lcd(void);
+void lcd_writeLine1 ( char text1[] );
+void lcd_writeLine2 ( char text2[] );
+void lcdShift(int displacement);
